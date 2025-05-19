7172f2137b29a98e374fcc4275c5b233afa62b1d
Your Name <you@example.com>
2025-05-19 21:26:17 +0800

    base page display

--- src/app/home/card/page.tsx
+++ src/app/home/card/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { FaCreditCard, FaStar ,FaHistory} from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import Card from 'components/card';
 import {
@@ -27,109 +27,157 @@ const Dashboard = () => {
   const cards: any[] = [
     {
       id: 1,
-      title: "TON",
-      subtitle: "Hello world",
+      title: "💎TON Card💎",
+      cardNum:"",
+      subtitle: "Best crypto prepaid card on TON",
       imgSrc: "/img/card/card-emp.png",
-      btnText: "Apply new card",
+      btnText: "Manage",
       onClick: () => {
-        alert("Card Management");
+        alert("Manage");
       },
     },
-    {
-      id: 2,
-      title: "BTC",
-      subtitle: "",
-      imgSrc: "/img/card/card-bg-1.webp",
-      btnText: "Card Management",
-      onClick: () => {
-        alert("Connect BTC clicked");
-      },
-    },
-    {
-      id: 3,
-      title: "ETH",
-      subtitle: "",
-      imgSrc: "/img/card/card-bg-2.jpg",
-      btnText: "Connect ETH",
-      onClick: () => {
-        alert("Connect ETH clicked");
-      },
-    },
+    // {
+    //   id: 2,
+    //   title: "BTC",
+    //   subtitle: "",
+    //   imgSrc: "/img/card/card-bg-1.webp",
+    //   btnText: "Apply",
+    //   onClick: () => {
+    //     alert("Apply new card");
+    //   },
+    // },
+    // {
+    //   id: 3,
+    //   title: "ETH",
+    //   cardNum:"Hello",
+    //   subtitle: "",
+    //   imgSrc: "/img/card/card-bg-2.jpg",
+    //   btnText: "Manage",
+    //   onClick: () => {
+    //     alert("Connect ETH clicked");
+    //   },
+    // },
   ];
 
+  const applyNowDisplay = (card:any)=>
+  {
+    if(card.cardNum){
+      return ("")
+    }
+    if(cards.length==1)
+    {
+      return (
+        <div className="w-full flex flex-row-reverse">
+                      <button
+                        onClick={
+                          ()=>
+                          {
+                            location.href = "/home/apply"
+                          }
+                        }
+                        className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded-md w-max"
+                      >
+                        Apply Now
+                      </button>
+        </div>
+      )
+    }
+    if(expanded)
+    {
+      return ("")
+    }
+
+  }
   return (
     <div>
 
       <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 justify-items-center">
         
-<div className="w-full max-w-md mx-auto py-8">
-   <h2 className="text-2xl font-bold text-white mb-4">My Poket</h2>
-      <div
-        className={`
-          relative 
-          ${expanded ? "h-auto overflow-visible" : "h-48 overflow-hidden"}
-          transition-all duration-300 ease-in-out
-        `}
-      >
-        {cards.map((card, idx) => (
+        <div className="w-full max-w-md mx-auto py-8">
+      <h2 className="text-2xl font-bold text-white mb-4 flex"><FaCreditCard/>&nbsp; My Poket &nbsp;</h2>
           <div
-            key={card.id}
             className={`
-              relative bg-gray-800 rounded-xl shadow-lg overflow-hidden
-              h-48
+              relative 
+              ${expanded ? "h-auto overflow-visible" : "h-48 overflow-hidden"}
               transition-all duration-300 ease-in-out
-              ${!expanded && idx > 0 ? "-mt-12" : "mt-0"}
             `}
           >
-            <div className="absolute inset-0">
-              <img
-                src={card.imgSrc}
-                alt={card.title}
-                className="w-full h-full object-cover"
-              />
-              <div className="absolute inset-0 bg-black/60"></div>
-            </div>
-            <div className="relative z-10 flex flex-col h-full justify-between p-4">
-              <div>
-                <h3 className="text-white text-xl font-semibold">
-                  {card.title}
-                </h3>
-                {card.subtitle && (
-                  <p className="text-gray-300 text-sm mt-1">
-                    {card.subtitle}
-                  </p>
-                )}
+            {cards.map((card, idx) => (
+              <div
+                key={card.id}
+                className={`
+                  relative bg-gray-800 rounded-xl shadow-lg overflow-hidden
+                  h-48
+                  transition-all duration-300 ease-in-out
+                  ${!expanded && idx > 0 ? "-mt-12" : "mt-0"}
+                `}
+              >
+                <div className="absolute inset-0">
+                  <img
+                    src={card.imgSrc}
+                    alt={card.title}
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="absolute inset-0 bg-black/60"></div>
+                </div>
+                <div className="relative z-10 flex flex-col h-full justify-between p-4">
+                  <div>
+                    <h3 className="text-white text-xl font-semibold">
+                      {card.title}
+                    </h3>
+                    {card.subtitle && (
+                      <p className="text-gray-300 text-sm mt-1">
+                        {card.subtitle}
+                      </p>
+                    )}
+                  </div>
+                  {
+                    applyNowDisplay(card)
+                  }
+                  {(card.btnText&&card.cardNum&&(expanded||cards.length<=1)) && (
+                    <button
+                      onClick={card.onClick}
+                      className="bg-gray-900 hover:bg-gray-700 text-white px-4 py-2 rounded-md w-max"
+                    >
+                      {card.btnText}
+                    </button>
+                  )}
+                </div>
               </div>
-
-              {(card.btnText&&expanded) && (
-                <button
-                  onClick={card.onClick}
-                  className="bg-gray-900 hover:bg-gray-700 text-white px-4 py-2 rounded-md w-max"
-                >
-                  {card.btnText}
-                </button>
-              )}
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-          {!expanded ? (
-            <div
-              onClick={() => setExpanded(true)}
-              className="mt-2 text-center text-gray-200 cursor-pointer select-none"
-            >
-              Tap to Expand
-            </div>
-          ) :
-          (
-            <div
-              onClick={() => setExpanded(false)}
-              className="mt-2 text-center text-gray-200 cursor-pointer select-none"
-            >
-              Tap to Close
-            </div>
-          )}
-    </div>
+              {!expanded ? (
+                <div
+                  onClick={() => setExpanded(true)}
+                  className="mt-2 text-center text-gray-200 cursor-pointer select-none"
+                  style={{
+                    display : cards.length>1 ? "":"none"
+                  }}
+                >
+                  Tap to Expand
+                </div>
+              ) :
+              (
+                <div
+                  onClick={() => setExpanded(false)}
+                  className="mt-2 text-center text-gray-200 cursor-pointer select-none"
+                  style={{
+                    display : cards.length>1 ? "":"none"
+                  }}
+                >
+                  Tap to Close
+                </div>
+              )}
+        </div>
+
+
+        <div className="w-full max-w-md mx-auto py-8">
+          <h2 className="text-2xl font-bold text-white mb-4 flex"><FaStar/>&nbsp; My Credit</h2>
+        </div>
+
+        <div className="w-full max-w-md mx-auto py-8">
+          <h2 className="text-2xl font-bold text-white mb-4 flex"><FaHistory/>&nbsp; Credit History</h2>
+        </div>
       </div>
     </div>
   );
